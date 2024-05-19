07297dfaa15e131914e0cc30bb89a3c46d9a85ab
kddjan <joelkddjan@gmail.com>
2024-05-19 13:26:37 +0000

    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -8,9 +8,33 @@ function processArray(arr) {
         }
         return processedArray;
     }
+
     
     
     let inputArray = [5, 10, 12, 18];
     let outputArray =
      processArray(inputArray);
     console.log(outputArray);
+
+        function formatArrayStrings(Strings, Numbers) {
+            const formattedStrings = [];
+            for (let i = 0; i < Strings.length; i++) {
+              const string = Strings[i];
+              const number = Numbers[i];
+              if (number % 2 === 0) {
+                formattedStrings.push(string.toUpperCase());
+              } else {
+                formattedStrings.push(string.toLowerCase());
+              }
+            }
+            return formattedStrings;
+          }
+          
+        
+        const originalStrings = ["Colin", "Green", "Black", "Nicolas",];
+        const processedNumbers = [5, 10, 12, 18]; 
+        
+        const formattedStrings = formatArrayStrings(originalStrings, processedNumbers);
+        
+        console.log("Original Strings:", originalStrings);
+        console.log("Formatted Strings:", formattedStrings);
